27d6b5ce845e0e403bf38aae085aa1fd7e8ed17d
Hewerton <hewertonlourenco@gmail.com>
2023-08-05 21:09:28 -0300

    Feat: add cofig for Image show the profile img

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/AAcHTteNZ-cPzjhOhk6qwNEiQi7xgoJ5zdnpOueeEkEbxJ5lnC8=s96-c',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
