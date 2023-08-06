a3624bdad28d118feb997b840d94f16b7ff0701b
Hewerton <hewertonlourenco@gmail.com>
2023-08-05 21:28:03 -0300

    Feat: create SessionProvider to wrap the SessionProvider of next-auth

---
+++ src/app/components/ProviderSession.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { SessionProvider } from "next-auth/react"
+import React from "react"
+
+interface Props {
+  children: React.ReactNode
+}
+
+export const ProviderSession = ({ children } : Props) => {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
